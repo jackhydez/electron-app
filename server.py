735ea95c0946d65e2e73fcb494685d46919de932
jackhydez <pash.ok92@yandex.ru>
2021-01-30 15:33:58 +0300

    add signal handler and cmd args

--- server.py
+++ server.py
@@ -1,16 +1,40 @@
+import signal
+
+import tornado.httpserver
 import tornado.ioloop
+import tornado.options
 import tornado.web
+
+from tornado.options import define, options
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("server say: 'Hello, world!'")
 
-def make_app():
-    return tornado.web.Application([
-        (r"/", MainHandler),
-    ])
+class MyApplication(tornado.web.Application):
+    is_closing = False
+
+    def signal_handler(self, signum, frame):
+        self.is_closing = True
+
+    def try_exit(self):
+        if self.is_closing:
+            tornado.ioloop.IOLoop.instance().stop()
+            print("Stop server")
+
+
+define("port", default=9999, help="run on the given port", type=int)
+
+app = MyApplication([
+    (r"/", MainHandler),
+])
 
 if __name__ == "__main__":
-    app = make_app()
-    app.listen(9999)
-    tornado.ioloop.IOLoop.current().start()
+    signal.signal(signal.SIGINT, app.signal_handler)
+    signal.signal(signal.SIGTERM, app.signal_handler)
+
+    tornado.options.parse_command_line()
+    app.listen(options.port)
+    tornado.ioloop.PeriodicCallback(app.try_exit, 100).start()
+    print("Start server")
+    tornado.ioloop.IOLoop.instance().start()
