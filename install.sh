722caaba6cd01e605e9418866dd1b700cc44c246
jackhydez <pash.ok92@yandex.ru>
2021-01-30 15:32:08 +0300

    fix sudo in install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install -y nodejs npm python3 python3-pip
+apt update && apt install -y nodejs npm python3 python3-pip
 
 pip3 install -r requirements.txt
 
