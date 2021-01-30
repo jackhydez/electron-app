335f202c5191d68a4f3da818d8b1556768c94f91
jackhydez <pash.ok92@yandex.ru>
2021-01-30 19:47:10 +0300

    add sudo in install bash

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt update && apt install -y nodejs npm python3 python3-pip
+sudo apt update && sudo apt install -y nodejs npm python3 python3-pip
 
 cd server
 pip3 install -r requirements.txt
