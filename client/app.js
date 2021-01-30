f4294417dce8257f1bdee23be32a9f324f27602c
jackhydez <pash.ok92@yandex.ru>
2021-01-30 21:06:33 +0300

    add setInterval fetch

--- client/app.js
+++ client/app.js
@@ -1,7 +1,27 @@
 
 let result = document.querySelector('#result')
 
-fetch('http://localhost:9999/')
-  .then(response => response.text())
-  .then(data => result.textContent = data);
+function setIntervalX(callback, delay, repetitions) {
+    var x = 0;
+    var intervalID = window.setInterval(function () {
 
+       callback();
+
+       if (++x === repetitions) {
+           window.clearInterval(intervalID);
+       }
+    }, delay);
+}
+
+function addTag(data) {
+    var tag = document.createElement("div");
+    var text = document.createTextNode(data);
+    tag.appendChild(text);
+    result.appendChild(tag);
+}
+
+setIntervalX(function () {
+    fetch('http://localhost:9999/')
+      .then(response => response.text())
+      .then(data => addTag(data));
+}, 1000, 5);
